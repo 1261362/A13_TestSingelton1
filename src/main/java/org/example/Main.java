c2d6825244c7e93cb5a9517a826e8f34cf56ad5c
1261362 <1261362@bdeb.qc.ca>
2024-06-20 15:22:04 -0400

    Ajout d'une class et main modifier

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,21 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bonjour le monde");
 
+        // Obtenir l'instance du singleton
+        Singleton s1 = Singleton.getInstance();
+        System.out.println("Singleton 1 : " + s1);
+
+        // Obtenir à nouveau l'instance du singleton
+        Singleton s2 = Singleton.getInstance();
+        System.out.println("Singleton 2 : " + s2);
+
+        // Vérifier si les deux références pointent vers la même instance
+        if (s1 == s2) {
+            System.out.println("Les deux instances sont identiques.");
+        } else {
+            System.out.println("Il existe différentes instances.");
         }
     }
+}
---
+++ src/main/java/org/example/Singleton.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Singleton {
+
+    // Instance unique de la classe
+    private static Singleton instance;
+
+    // Constructeur privé pour empêcher l'instanciation
+    private Singleton() {
+    }
+
+    // Méthode publique pour fournir l'accès à l'instance
+    public static Singleton getInstance() {
+        if(instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
