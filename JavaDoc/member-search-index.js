dd9446fe0aadceb99222fcf4fc9a7237bc36c74e
1261362 <1261362@bdeb.qc.ca>
2024-06-27 14:55:22 -0400

    AJout de JavaDOc

--- JavaDoc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"ex1","c":"Singleton","l":"getInstance()"},{"p":"ex1","c":"Main","l":"Main()","u":"%3Cinit%3E()"},{"p":"ex1","c":"Main","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
--- JavaDoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ex1","l":"Main"},{"p":"ex1","l":"Singleton"}];updateSearchResults();
